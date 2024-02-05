9cea7eb3843f957f15b763aa33cfaefa33dc1dd4
Hasan M. Hallak <hasanm.hallak@gmail.com>
2024-02-05 21:41:48 +0200

    add library docs

--- lib/eitherx.dart
+++ lib/eitherx.dart
@@ -1,3 +1,11 @@
+/// `Either` is a generic class in Dart that represents a value of
+/// one of two possible types: `L` or `R`. The class is useful when
+/// you want to return a value that can either be of type `L` or of
+/// type `R`, but not both.
+
+/// This class is inspired by the Either data type in functional
+/// programming and provides a way to handle errors and exceptions
+/// in a more expressive and concise way.
 library eitherx;
 
 export 'src/either.dart';
